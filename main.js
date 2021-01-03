8a44ba7ac6e60040cdd72e77507d36a7f7bb091b
zimniok <pierzchala.ma@gmail.com>
2021-01-04 00:46:23 +0100

    Chenged Interval to Timeout

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const GameSense = require("./gameSense.js")
 var headsetCreds = ListHeadsets.getConnectedHeadset();
 var myHeadset = new Headset(headsetCreds)
 var gs = new GameSense()
+
 let recuringFunction = function(){
   let battery_percent = myHeadset.getBatteryPercentage()
   gs.sendBP(battery_percent)
+  setTimeout(recuringFunction, 5000)
 }
 
-setInterval(recuringFunction, 5000)
+recuringFunction()
